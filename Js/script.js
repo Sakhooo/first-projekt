fd1dda246292a06e1cf20493353ffc1d120fe8fc
Sagadat <sako.07.1997@gmail.com>
2021-12-20 00:29:44 +0600

    first commit

---
+++ Js/script.js
@@ -0,0 +1,44 @@
+// let number = 4.5;
+// console.log(-4/0);
+// console.log('Number: '*0);
+
+// const obj={
+//     name: 'Sako',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// console.log(obj['name']);
+
+// let arr = ['sako',6,[],{}];
+// console.log(arr[1]);
+
+
+// const result = confirm("are you sure you want to?");
+// console.log(result);
+
+
+// const answer = +prompt("hi you","");
+// console.log(typeof(answer));
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("как ваше имя","");
+// answers[1] = prompt("как ваше фамиля","");
+// answers[2] = prompt("возрасть","");
+
+// document.write(answers);
+
+// console.log(typeof(answers));
+
+
+
+const category ='toys';
+console.log(`http//someurl/${category}/5`);
+
+
+const user = "Sagadat";
+alert(`hello, ${user}`);
